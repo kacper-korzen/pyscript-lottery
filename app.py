470978f44a858c9d2daac483474b4259b157cca3
Kacper K <kacper-korzen@users.noreply.github.com>
2025-05-27 13:46:07 +0200

    delete html strings with divs of the screens

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from js import document
+from pyscript import document
 
 class StartScreen():
     pass
@@ -25,15 +25,28 @@ selection_html = """
 <div class="selection-screen">
     <h2>Wybierz 6 swoich szczęścliwych liczb(1-49):</h2>
     <div class="number-inputs">
-        <input type="text" id="num1" min="1" max="49">
-        <input type="text" id="num2" min="1" max="49">
-        <input type="text" id="num3" min="1" max="49">
-        <input type="text" id="num4" min="1" max="49">
-        <input type="text" id="num5" min="1" max="49">
-        <input type="text" id="num6" min="1" max="49">
+        <input type="number" id="num1" min="1" max="49" value="1">
+        <input type="number" id="num2" min="1" max="49" value="1">
+        <input type="number" id="num3" min="1" max="49" value="1">
+        <input type="number" id="num4" min="1" max="49" value="1">
+        <input type="number" id="num5" min="1" max="49" value="1">
+        <input type="number" id="num6" min="1" max="49" value="1">
     </div>
     <button id="draw-btn" class="btn btn-success" disabled>Losuj</button>
 
 </div>
 """
+
+app_div = document.querySelector("#app")
+
+def changeingScreens():
+    while True:
+        play_btn = document.querySelector("#play-btn") 
+        if play_btn:
+            play_btn.onclick = lambda e: setattr(app_div, 'innerHTML', selection_html)
+
+
+        draw_btn = document.querySelector("#draw-btn")
+        if draw_btn:
+            draw_btn.onclick = lambda e: setattr(app_div, 'innerHTML', result_html)
 

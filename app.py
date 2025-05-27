00ee10a46ccec7d1a76e776576cba61cee4330ee
Kacper K <kacper-korzen@users.noreply.github.com>
2025-05-27 23:39:15 +0200

    add on click function to draw button

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pyscript import document as d
+from js import alert
 
 
 class StartScreen:
@@ -13,9 +14,14 @@ class ResultScreen:
     pass
 
 
-playBtn = d.querySelector("#play-btn")
 selectionDiv = d.querySelector(".selection-screen")
 startDiv = d.querySelector(".start-screen")
+resultDiv = d.querySelector(".result-screen")
+
+playBtn = d.querySelector("#play-btn")
+drawBtn = d.querySelector("#draw-btn")
+
+pickedNumbers = set()
 
 
 def onClickStart(e):
@@ -26,3 +32,30 @@ def onClickStart(e):
 
 
 playBtn.onclick = onClickStart
+
+
+def is_int(value):
+    try:
+        int(value, 10)
+        return True
+    except ValueError:
+        return False
+
+
+def onClickSelection(e):
+    for input in d.querySelectorAll("input"):
+        tmp = input.value
+        if is_int(tmp):
+            pickedNumbers.add(int(tmp))
+        else:
+            alert("Sprawdź poprawność liczb!")
+            pickedNumbers.clear()
+
+    selectionDiv.classList.remove("d-flex,flex-column")
+    selectionDiv.classList.add("d-none")
+    resultDiv.remove("d-none")
+
+
+drawBtn.onclick = onClickSelection
+
+
